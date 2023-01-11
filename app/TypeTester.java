52e6ce9ede1ad39fa8c6ad18467564c943d71d05
Lurg <93657485+Lurg01@users.noreply.github.com>
2023-01-12 00:10:36 +0800

    Add files via upload
    Upload App File

---
+++ app/TypeTester.java
@@ -0,0 +1,24 @@
+
+
+public class TypeTester {
+
+    public TypeTester(){
+
+        void printType(byte x) {
+            System.out.println(x + " is an byte");
+        }
+        void printType(int x) {
+            System.out.println(x + " is an int");
+        }
+        void printType(float x) {
+            System.out.println(x + " is an float");
+        }
+        void printType(double x) {
+            System.out.println(x + " is an double");
+        }
+        void printType(char x) {
+            System.out.println(x + " is an char");
+        }
+    }
+
+}
